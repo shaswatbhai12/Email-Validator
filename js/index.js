0b846cbf2360619ca6cb3c8ba33157c85abd5a80
shaswat <shaswatkumar9868@gmail.com>
2026-07-15 20:33:10 +0530

    Almost completed the UI

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
 console.log("This is my script")
+
+let result = {
+  "tag": "",
+  "free": false,
+  "role": false,
+  "user": "shaswat",
+  "email": "shaswat@starliftandcontroller.xyz",
+  "score": 0.8,
+  "state": "deliverable",
+  "domain": "starliftandcontroller.xyz",
+  "reason": "low_deliverability",
+  "mx_found": true,
+  "catch_all": null,
+  "disposable": false,
+  "smtp_check": true,
+  "did_you_mean": "",
+  "format_valid": true
+}
